a016fbb6d8459f309f53ea35773ecb612590233c
GHOST2323qwert <luislage19999@gmail.com>
2026-04-07 11:00:31 +0100

    Update SettingsActivity.kt

--- app/src/main/java/com/impactsos/SettingsActivity.kt
+++ app/src/main/java/com/impactsos/SettingsActivity.kt
@@ -1,17 +1,12 @@
+
 package com.sosimpact
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val text = TextView(this)
-        text.text = "⚙️ Definições\n\nModo escuro (em breve)"
-        text.textSize = 20f
-
-        setContentView(text)
+        setContentView(android.R.layout.simple_list_item_1)
     }
 }
