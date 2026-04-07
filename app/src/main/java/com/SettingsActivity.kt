e0a633cc9b9cb8189c5e4dc87c9468a652296d7d
GHOST2323qwert <luislage19999@gmail.com>
2026-04-07 12:57:37 +0100

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.sosimpact
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(android.R.layout.simple_list_item_1)
+
+    }
+
+}
